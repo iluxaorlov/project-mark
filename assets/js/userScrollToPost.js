b90bdf4b9089b23031feb7b32cf51f9ea030c51e
Илья Орлов <iluxaorlov@yandex.ru>
2019-07-13 08:28:58 +0300

    fix scrolling to posts

--- assets/js/userScrollToPost.js
+++ assets/js/userScrollToPost.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
     $('.profile__detail__item').filter(':first').on('click', function(event) {
         event.preventDefault();
 
-        let $scrollTop = $('#posts').offset().top - 64;
+        let $scrollTop = $('#posts').offset().top - 67;
         $('html, body').animate({
             scrollTop: $scrollTop
         }, 250);
