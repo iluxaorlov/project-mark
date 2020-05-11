8eb0b3e968b83efbcf4b174065cfda997223d7fb
Ilya Orlov <iluxaorlov@yandex.ru>
2020-05-11 23:11:03 +0300

    added styles

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+const Encore = require('@symfony/webpack-encore');
+
+Encore
+    .setOutputPath('public/build/')
+    .setPublicPath('/build')
+    .addEntry('app', './assets/js/app.js')
+    .splitEntryChunks()
+    .enableSingleRuntimeChunk()
+    .cleanupOutputBeforeBuild()
+    .enableSassLoader()
+;
+
+module.exports = Encore.getWebpackConfig();
