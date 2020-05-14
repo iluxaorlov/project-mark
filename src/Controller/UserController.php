7d78855d36d9342de0656e8623361a77575a5dbc
Ilya Orlov <iluxaorlov@yandex.ru>
2020-05-14 23:59:11 +0300

    added settings

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -4,8 +4,10 @@ namespace App\Controller;
 
 use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class UserController extends AbstractController
 {
@@ -21,6 +23,47 @@ class UserController extends AbstractController
         return $this->render('user/profile.html.twig', [
             'user' => $user,
             'posts' => $user->getPosts(),
+        ]);
+    }
+
+    /**
+     * @Route("/settings", name="settings")
+     *
+     * @param Request $request
+     * @param ValidatorInterface $validator
+     *
+     * @return Response
+     */
+    public function settings(Request $request, ValidatorInterface $validator)
+    {
+        /** @var User $user */
+        $user = $this->getUser();
+        $error = null;
+
+        if ($request->getMethod() === Request::METHOD_POST) {
+            $user->setUsername($request->get('username'));
+            $user->setFullName($request->get('full_name'));
+            $user->setAbout($request->get('about'));
+            $errors = $validator->validate($user);
+
+            if ($errors->count() > 0) {
+                $error = $errors->get(0);
+            }
+
+            if ($errors->count() < 0) {
+                $entityManager = $this->getDoctrine()->getManager();
+                $entityManager->persist($user);
+                $entityManager->flush();
+
+                return $this->redirectToRoute('profile', [
+                    'username' => $user->getUsername(),
+                ]);
+            }
+        }
+
+        return $this->render('user/settings.html.twig', [
+            'user' => $user,
+            'error' => $error,
         ]);
     }
 }
